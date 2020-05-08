18934dd4d7c2d85ed4a546e137544fea8b8dce88
Agnes <aggyreina@gmail.com>
2020-05-08 17:14:59 +0200

    created a simple todo app with react

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import AddTodo from './addTodo'
 class App extends Component {
   state = {
     todos: [
-      { id: 1, content: 'buy some milk' },
-      { id: 2, content: 'play cards' }
+      { id: 1, content: 'buy decoration materials' },
+      { id: 2, content: 'play cards' },
+      { id: 3, content: 'level up in react' }
     ]
   }
   deleteTodo = (id) => {
