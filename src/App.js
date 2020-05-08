d4ddb10f59240db7d72f32576542addc860741d3
Agnes <aggyreina@gmail.com>
2020-05-08 17:04:33 +0200

    created a simple todo app with react

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="app container">
-        <h1 className="center maganta-text">Todo App</h1>
+        <h1 className="center green-text">Todo App</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
         <AddTodo addTodo={this.addTodo} />
       </div>
